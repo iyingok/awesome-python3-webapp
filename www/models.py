a3d48cf440f103a110b86ba75e256148148ee78f
QinOffice <iyingok@dl.cn>
2020-06-04 14:36:28 +0800

    wait

--- www/models.py
+++ www/models.py
@@ -54,12 +54,14 @@ if __name__ == "__main__":
     async def test():
         await orm.create_pool(user='www-data', password='www-data', db='awesome')
 
-        u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+        u = User(name='Test', email='test2@example.com', passwd='1234567890', image='about:blank')
 
         await u.save()
 
     loop = asyncio.get_event_loop()
 
     loop.run_until_complete(test())
-    loop.close()
+    loop.run_forever()
+
+    #loop.close()
 
